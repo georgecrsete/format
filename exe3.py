265bc9645faf322a0fb15ed4336e766dcf64221a
George <george.p@escolar.ifrn.edu.br>
2025-09-17 17:58:36 -0300

    Create exe3.py

---
+++ exe3.py
@@ -0,0 +1,14 @@
+
+valor = float(input("Digite o valor da compra: R$ "))
+
+vista = valor * 0.91  
+
+
+prestacao5 = valor / 5
+
+
+prestacao10 = (valor * 1.17) / 10  
+
+print(vista)
+print(prestacao5)
+print(prestacao10)
